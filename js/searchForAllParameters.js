86d9c8957ff01b2ada3536702c537506f3a5fd02
YanBtd <ybertrand@free.fr>
2023-06-01 02:38:25 +0200

    Commit #5: Implementation of 3 characters minimum in the search bar to launch the research == OK

--- js/searchForAllParameters.js
+++ js/searchForAllParameters.js
@@ -11,20 +11,30 @@ export function searchForAllParameters(getRecipesJSON) {
     
     searchInput.addEventListener("input", filterData);
 
+    // searchInput.addEventListener("blur", (event) => {
+    //     event.target.value = "";
+    // });
+
     function filterData(e) {
+        // On lance la recherche à partir de 3 caractères tapés dans la barre de recherches
+        if(e.target.value.length > 2) {
+            const recipesList = document.querySelector(".recipes-list");
+            recipesList.textContent = "";
 
-        const recipesList = document.querySelector(".recipes-list");
-        recipesList.textContent = "";
+            const searchedString = e.target.value.toLowerCase();
 
-        const searchedString = e.target.value.toLowerCase();
-
-        const filteredArr = getRecipesJSON().filter(
-            recipeData => recipeData.name.toLowerCase().includes(searchedString)
-            || recipeData.description.toLowerCase().includes(searchedString)
-            || recipeData.ingredients.some(el => el.ingredient.toLowerCase().includes(searchedString))
-            || recipeData.appliance.toLowerCase().includes(searchedString)
-            || recipeData.ustensils.some(el => el.toLowerCase().includes(searchedString)))
-        ;
-        displayAllRecipes(filteredArr);
-    } 
+            const filteredArr = getRecipesJSON().filter(
+                recipeData => recipeData.name.toLowerCase().includes(searchedString)
+                || recipeData.description.toLowerCase().includes(searchedString)
+                || recipeData.ingredients.some(el => el.ingredient.toLowerCase().includes(searchedString))
+                || recipeData.appliance.toLowerCase().includes(searchedString)
+                || recipeData.ustensils.some(el => el.toLowerCase().includes(searchedString)))
+            ;
+            displayAllRecipes(filteredArr);
+        }
+        // Si la barre de recherche est vidée, on réaffiche l'ensemble des recettes
+        if(e.target.value.length == 0) {
+            displayAllRecipes(getRecipesJSON());
+        }
+    }
 };
